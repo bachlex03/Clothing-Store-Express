99665ebf4c8e5c3bf3ffc6bfe3c451a00ae9cda1
Nhat Dang <nhatdzfbk@gmail.com>
2024-11-21 03:25:16 +0700

    Nhat fixed

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -6,159 +6,162 @@ const categoryModel = require("../models/category.model");
 const productModel = require("../models/product.model");
 
 const create = async (body) => {
-  let { name = "", parentId = null } = body;
+    let { name = "", parentId = null } = body;
 
-  try {
-    const category = await categoryModel.create({
-      category_name: name,
-      category_parentId: parentId,
-    });
+    try {
+        const category = await categoryModel.create({
+            category_name: name,
+            category_parentId: parentId,
+        });
 
-    return category;
-  } catch (err) {
-    console.error(err);
-  }
+        return category;
+    } catch (err) {
+        console.error(err);
+    }
 
-  return null;
+    return null;
 };
 
 const update = async (id, body) => {
-  const { name, parentId } = body;
+    const { name, parentId } = body;
 
-  if (!id) {
-    throw new BadRequestError("Invalid category id");
-  }
+    if (!id) {
+        throw new BadRequestError("Invalid category id");
+    }
 
-  const category = await categoryModel.findById(id);
+    const category = await categoryModel.findById(id);
 
-  if (!category) {
-    throw new NotFoundError("Category not found");
-  }
+    if (!category) {
+        throw new NotFoundError("Category not found");
+    }
 
-  category.category_name = name;
-  category.category_parentId = parentId;
+    category.category_name = name;
+    category.category_parentId = parentId;
 
-  await category.save();
+    await category.save();
 
-  return category;
+    return category;
 };
 
 const remove = async (id) => {
-  if (!id) {
-    throw new BadRequestError("Invalid category id");
-  }
+    if (!id) {
+        throw new BadRequestError("Invalid category id");
+    }
 
-  const category = await categoryModel.findByIdAndDelete(id);
+    const category = await categoryModel.findByIdAndDelete(id);
 
-  if (!category) {
-    throw new NotFoundError("Category not found");
-  }
+    if (!category) {
+        throw new NotFoundError("Category not found");
+    }
 
-  return !!category;
+    return !!category;
 };
 
 const getAll = async () => {
-  try {
-    const categories = await categoryModel.find(
-      {},
-      {
-        category_name: 1,
-        category_parentId: 1,
-        category_slug: 1,
-      }
-    );
+    try {
+        const categories = await categoryModel.find(
+            {},
+            {
+                category_name: 1,
+                category_parentId: 1,
+                category_slug: 1,
+            }
+        );
 
-    return categories;
-  } catch (err) {
-    console.error(err);
-  }
+        return categories;
+    } catch (err) {
+        console.error(err);
+    }
 
-  return null;
+    return null;
 };
 
 const withChildren = async () => {
-  try {
-    const categories = await categoryModel.aggregate([
-      {
-        $match: {
-          category_parentId: null,
-        },
-      },
-      {
-        $lookup: {
-          from: "categories",
-          localField: "_id",
-          foreignField: "category_parentId",
-          as: "children",
-        },
-      },
-      {
-        $addFields: {
-          childrenDebug: "$children",
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          parent: "$category_name",
-          children: {
-            $map: {
-              input: "$children",
-              as: "child",
-              in: "$$child.category_name",
+    try {
+        const categories = await categoryModel.aggregate([
+            {
+                $match: {
+                    category_parentId: null,
+                },
             },
-          },
-        },
-      },
-    ]);
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "_id",
+                    foreignField: "category_parentId",
+                    as: "children",
+                },
+            },
+            {
+                $addFields: {
+                    childrenDebug: "$children",
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    parent: "$category_name",
+                    children: {
+                        $map: {
+                            input: "$children",
+                            as: "child",
+                            in: "$$child.category_name",
+                        },
+                    },
+                },
+            },
+        ]);
 
-    console.log(JSON.stringify(categories, null, 2)); // Log intermediate results
+        console.log(JSON.stringify(categories, null, 2)); // Log intermediate results
 
-    return categories;
-  } catch (err) {
-    console.error(err);
-  }
+        return categories;
+    } catch (err) {
+        console.error(err);
+    }
 
-  return null;
+    return null;
 };
 
 const getProductsByCategory = async (params) => {
-  const { slug } = params;
+    const { slug } = params;
 
-  if (!slug) {
-    throw new BadRequestError("Invalid category slug");
-  }
+    if (!slug) {
+        throw new BadRequestError("Invalid category slug");
+    }
 
-  // Tìm category hiện tại
-  const currentCategory = await categoryModel.findOne({ category_slug: slug });
-  if (!currentCategory) {
-    throw new BadRequestError("Category not found");
-  }
+    // Tìm category hiện tại
+    const currentCategory = await categoryModel.findOne({ category_slug: slug });
+    if (!currentCategory) {
+        throw new BadRequestError("Category not found");
+    }
 
-  // Tìm tất cả category con
-  const childCategories = await categoryModel.find({
-    category_parentId: currentCategory._id,
-  });
+    // Tìm tất cả category con
+    const childCategories = await categoryModel.find({
+        category_parentId: currentCategory._id,
+    });
 
-  // Tạo mảng chứa id của category hiện tại và các category con
-  const categoryIds = [currentCategory._id];
-  childCategories.forEach((child) => {
-    categoryIds.push(child._id);
-  });
+    // Tạo mảng chứa id của category hiện tại và các category con
+    const categoryIds = [currentCategory._id];
+    childCategories.forEach((child) => {
+        categoryIds.push(child._id);
+    });
 
-  // Lấy sản phẩm từ tất cả categories
-  const products = await productModel.find({
-    product_category: { $in: categoryIds },
-  });
+    // Lấy sản phẩm từ tất cả categories và populate thông tin category
+    const products = await productModel.find({
+        product_category: { $in: categoryIds },
+    }).populate({
+        path: 'product_category',
+        select: 'category_name category_slug'
+    });
 
-  return products;
+    return products;
 };
 
 module.exports = {
-  create,
-  getAll,
-  withChildren,
-  getProductsByCategory,
-  update,
-  remove,
+    create,
+    getAll,
+    withChildren,
+    getProductsByCategory,
+    update,
+    remove,
 };

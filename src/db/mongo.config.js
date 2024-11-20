e36995bf9b9882dde0e821d7713d9f6d6ff1e6e7
Nhat Dang <nhatdzfbk@gmail.com>
2024-11-21 03:25:16 +0700

    Nhat fixed

--- src/db/mongo.config.js
+++ src/db/mongo.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const {
-  db: { host, port, name },
+    db: { host, port, name },
 } = require("../config/config.env");
 
 // const CONNECTION_STR = `mongodb://${host}:${port}/${name}`;
@@ -12,34 +12,34 @@ console.warn("port::", port);
 console.warn("name::", name);
 
 class Database {
-  instance = null;
+    instance = null;
 
-  constructor() {
-    Database.connect();
-  }
-
-  static async connect(type = "mongodb") {
-    console.warn("CONNECTION_STR::", CONNECTION_STR);
-
-    await mongoose
-      .connect(CONNECTION_STR, { maxPoolSize: 50 })
-      .then((connection) => {
-        this.instance = connection;
-
-        console.log("Connected to database");
-      })
-      .catch((_) => {
-        console.log("Failure connecting to database");
-      });
-  }
-
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = new Database();
+    constructor() {
+        Database.connect();
     }
 
-    return this.instance;
-  }
+    static async connect(type = "mongodb") {
+        console.warn("CONNECTION_STR::", CONNECTION_STR);
+
+        await mongoose
+            .connect(CONNECTION_STR, { maxPoolSize: 50 })
+            .then((connection) => {
+                this.instance = connection;
+
+                console.log("Connected to database");
+            })
+            .catch((_) => {
+                console.log("Failure connecting to database");
+            });
+    }
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new Database();
+        }
+
+        return this.instance;
+    }
 }
 
 module.exports = Database;
